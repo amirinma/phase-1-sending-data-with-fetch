08a5dec043a7054dc2330f4f6ce1c1da6dcce22e
Mohammad <zia.ro@live.com>
2022-05-07 16:51:26 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Add your code here
+// const formData = {
+//     userName: "Byron",
+//     email: "Poodle",
+//   };
+  
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   })
+//   .catch(function (error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   });
+function submitData(userName, email){
+    const info = {name: userName, contact: email}
+    const request = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        body: JSON.stringify(info) 
+    }
+    fetch("http://localhost:3000/users", request)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+        console.log(object);
+    })
+    .catch(function(err){
+        alert("There is error!")
+    })
+}
