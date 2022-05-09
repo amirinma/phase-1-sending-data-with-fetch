507fc40a768a0dc7012ae38131aa77033728bcad
Mohammad <zia.ro@live.com>
2022-05-08 22:50:19 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,47 +1,28 @@
-// Add your code here
-// const formData = {
-//     userName: "Byron",
-//     email: "Poodle",
-//   };
-  
-//   const configurationObject = {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Accept: "application/json",
-//     },
-//     body: JSON.stringify(formData),
-//   };
-  
-//   fetch("http://localhost:3000/dogs", configurationObject)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (object) {
-//     console.log(object);
-//   })
-//   .catch(function (error) {
-//     alert("Bad things! Ragnarők!");
-//     console.log(error.message);
-//   });
-function submitData(userName, email){
-    const info = {name: userName, contact: email}
-    const request = {
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email,
+       
+      };
+      const configurationObject = {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-        body: JSON.stringify(info) 
-    }
-    fetch("http://localhost:3000/users", request)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(object){
-        console.log(object);
-    })
-    .catch(function(err){
-        alert("There is error!")
-    })
-}
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData)
+       
+      };
+    
+     return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (object) {
+          console.log(object);
+          document.body.innerHTML = ` ${object.id}` 
+        })
+        .catch ((error) => {
+            console.log(error.message)
+            document.body.innerHTML = error.message
+    })}
